75309972f9322a0a0a71f1df63e4cadacfb2c124
Tresh <5liltresh@gmail.com>
2025-05-27 01:21:39 +0000

    incomplete menu 2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import Logo from "../assets/treshteam_logo.svg";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Button from "./Button/Button";
 import { Menu } from 'lucide-react';
+import { FaHandHoldingMedical } from 'react-icons/fa6';
 
 const NavBar = () => {
   const location = useLocation();
@@ -13,11 +14,16 @@ const NavBar = () => {
     setMenuOpen(!menuOpen)
   }
   return (
-    <div className="flex items-center justify-between px-7 md:px-16 py-3">
-      <ul>
+    <div className="relative flex items-center justify-between px-7 md:px-16 py-3">
+      <ul className="flex items-center justify-between w-full">
         <li className="flex items-center Logo space-x-1">
           <img src={Logo} className="w-[40px] md:h-[50px] h-[40px] md:h-[50px]" alt="" />
           <h1 className="text-md md:text-xl font-semibold uppercase">Team Tresh</h1>
+        </li>
+        <li>
+        <div onClick={handleMenu}>
+          <Menu/>
+        </div>
         </li>
       </ul>
       <ul className="hidden md:flex items-center md:space-x-10 text-lg font-medium">
@@ -86,9 +92,6 @@ const NavBar = () => {
       {/* <button className="bg-primary text-white rounded-3xl py-2.5 px-5">
         Contact Us
       </button> */}
-      <div onClick={handleMenu}>
-        <Menu/>
-      </div>
       <Button
         className="hidden md:inline-block shadow-md bg-[#1d4ed8] text-white text-sm md:text-md py-2.5 px-5 rounded-[30px]"
         onClick={() => navigate('/contact-us')}>Contact Us</Button>
