d732b3be8447e54f4494859bc28e4445cb16224b
Tresh <5liltresh@gmail.com>
2025-08-06 21:46:24 +0100

    Update TeamData.jsx

--- src/Pages/Home/TeamData.jsx
+++ src/Pages/Home/TeamData.jsx
@@ -1,8 +1,8 @@
-import Tomiwa from "../../assets/Images/tomiwa.png";
+import Tomiwa from "../../assets/Images/tomiwa.jpg";
 import Martins from "../../assets/Images/martins.jpg";
-import Okay from "../../assets/Images/okay.png";
+import Okay from "../../assets/Images/okay.jpg";
 import JoeCode from "../../assets/Images/joecode.jpg";
-import JoshDraws from "../../assets/Images/joshdraws.png";
+import JoshDraws from "../../assets/Images/joshdraws.jg";
 import Alrak from "../../assets/Images/alrak.jpg";
 
 export const teamData = [
